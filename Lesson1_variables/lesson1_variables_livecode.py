09397dab925176e67c2f1687da9c64c9912d0f4a
Rachel Worthman <rachelworthman@gmail.com>
2018-11-17 14:52:06 -0800

    added variables python livecode

---
+++ Lesson1_variables/lesson1_variables_livecode.py
@@ -0,0 +1,13 @@
+## Lesson 1: Variables
+## Author: GWC PDX
+## Last Edit: 
+## Description: intro to variables
+
+name="Molly"
+print(name)
+Name="Priya"
+print(Name)
+appLES="7"
+print(appLES)
+apples_3=3
+print(apples_3)
