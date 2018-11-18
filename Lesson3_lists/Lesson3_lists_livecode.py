9d72056a3472038836fa739b30af1c861a48c1df
Priya Bhatt <priyabhatt09@gmail.com>
2018-11-18 15:44:05 -0800

    Create Lesson3_lists_livecode.py

---
+++ Lesson3_lists/Lesson3_lists_livecode.py
@@ -0,0 +1,22 @@
+# Lesson 3 - Lists - live coding examples
+
+# Indexing Lists
+back_to_school = ["pens", "calculator", "highlighter", "notebook"]
+back_to_school[1]
+back_to_school[3]
+back_to_school[0]
+back_to_school[-1]
+
+# Indexing lists when lists are in lists!
+crazy_list = [[1,2,3,4] , 4.5, "wow, lists are amazing!", ["I", "love", "this", "list"], 10000]
+
+len(crazy_list)
+
+# Indexing Lists in lists!
+crazy_list[0]
+crazy_list[0][1]
+crazy_list[2]
+crazy_list[-1]
+crazy_list[3]
+
+len(crazy_list[0])
