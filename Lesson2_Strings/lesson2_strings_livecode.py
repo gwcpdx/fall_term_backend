2b58405abf19981f319b594b0c878e5aba08479e
Priya Bhatt <priyabhatt09@gmail.com>
2018-11-17 16:11:19 -0800

    creating lesson 2 strings live code examples

---
+++ Lesson2_Strings/lesson2_strings_livecode.py
@@ -0,0 +1,48 @@
+## Lesson 2: Strings - Live Coding Examples
+
+# Printing Strings
+print("Hi Rachel!")
+print("I am a string.")
+print("I have double quotes!")
+print('But I can also have single quote!')
+print("But sometimes you have tobe careful!")
+
+# Printing spaces
+print (" ")
+
+# Print a quotation mark
+print("Now I can print \" in my string!")
+
+# Combining strings
+print("Jenn " + "is " + "fun! ")
+
+print("Jenn" * 10)
+
+# Assigning strings to variables
+str_1 = "I want the "
+str_2 = "bouncy "
+str_3 = "ball"
+
+print(str_1 + str_2 + str_3)
+print ( str_1 + str_2 * 6 + str_3)
+
+# Indexing Strings
+pizza = "Hawaiian"
+print(pizza[0])
+print(pizza[2:5])
+
+# Checking the length of a string
+#   ie: checking how many characters are in a string
+print(len(pizza))
+
+# Assigning strings to variables
+Jenn = "My favorite color is BLUE!"
+Rachel = "My favorite sport is GOLF!"
+Molly = "My favorite book is Revolutionary Road!"
+Priya = "My favorite movie is 'The Devil Wears Prada'!"
+Josh = "Where's Josh?"
+
+# Taking a string input from the user and assigning it to a variable
+fullname = input("What is your full name? ")
+
+print(fullname)
