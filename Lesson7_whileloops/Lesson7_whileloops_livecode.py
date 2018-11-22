8ff683e80269f829b2e1174a8ff424a063e24778
Priya Bhatt <priyabhatt09@gmail.com>
2018-11-21 21:46:41 -0800

    Create Lesson7_whileloops_livecode.py

---
+++ Lesson7_whileloops/Lesson7_whileloops_livecode.py
@@ -0,0 +1,11 @@
+# Lesson 7 - while loops live coding examples
+
+# Initialize a counter
+number = 0
+
+while number < 10:
+   number = number + 1
+   print number
+  
+# What is number outside of the while loop after you run it?  Does it change from 0?
+print number
